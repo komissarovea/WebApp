3bc69d28a67411f3825a19eb48a1d88f94e7d2fd
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-09 18:28:05 +0300

    minimal api

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +12,68 @@ builder.Services.AddDbContext<DataContext>(opts =>
 
 var app = builder.Build();
 
+const string BASEURL = "api/products";
+
+app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+{
+    string? id = context.Request.RouteValues["id"] as string;
+    if (id != null)
+    {
+        Product? p = data.Products.Find(long.Parse(id));
+        if (p == null)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+        else
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(p));
+        }
+    }
+});
+
+app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+{
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
+});
+
+app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+{
+    Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+    if (p != null)
+    {
+        await data.AddAsync(p);
+        await data.SaveChangesAsync();
+        context.Response.StatusCode = StatusCodes.Status200OK;
+    }
+});
+
 app.UseMiddleware<WebApp.TestMiddleware>();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return "Hello World!";
+});
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
 
 app.Run();
+
+// app.Use(async (HttpContext context, Func<Task> next) =>
+// {
+//     //await Task.CompletedTask;
+//     await next();
+// });
+
+//Action<Task> a;
+//Predicate<Task> p;
+// app.Use(async (HttpContext context, RequestDelegate next) =>
+// {
+//     await next(context);
+// });
+// app.Run((HttpContext context) =>
+// {
+//     return Task.CompletedTask;
+// });
