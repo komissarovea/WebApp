36c16e5695c74b5abc500948ac33a4151429b653
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-10 16:39:11 +0300

    model binding from route and query

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,16 +14,18 @@ namespace WebApp.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetProducts()
+        public IEnumerable<Product> GetProducts(long? id)
         {
-            return context.Products;
+            var product = context.Products.Find(id);
+            return product != null ? new[] { product } : context.Products;
         }
 
         [HttpGet("{id}")]
-        public Product? GetProduct([FromServices] ILogger<ProductsController> logger)
+        public Product? GetProduct(long id, [FromServices] ILogger<ProductsController> logger)
         {
             logger.LogDebug("GetProduct Action Invoked");
-            return context.Products.FirstOrDefault();
+            return context.Products.Find(id);
+            //return context.Products.FirstOrDefault();
         }
     }
 }
