eba4d16736cc708f9462285e24f5dfd0df72495f
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-12 12:06:37 +0300

    corse sample

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
+// https://learn.microsoft.com/en-gb/aspnet/core/security/cors?view=aspnetcore-7.0
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(opts =>
@@ -11,7 +14,20 @@ builder.Services.AddDbContext<DataContext>(opts =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                      });
+});
+
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
