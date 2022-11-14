eb894dc26727ccdd8f8d0471554231edc0b6a16a
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-14 13:12:59 +0300

    custom authorization filter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Filters;
 
 namespace WebApp.Controllers
 {
-    [RequireHttps]
+    // [RequireHttps]
+    [HttpsOnly]
     public class HomeController : Controller
     {
         // [RequireHttps]
